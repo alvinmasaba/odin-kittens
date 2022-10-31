fe3233941e67ccd462b9fd485194faafcd990fed
funkyman3121 <alvinmasaba@gmail.com>
2022-10-31 13:32:52 -0400

    Adds validators with custom messages to kittens model.

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,3 +1,6 @@
 class Kitten < ApplicationRecord
-  validates :name, :age, presence: true
+  validates :name, presence: { message: "You really must be dumb forgetting the name of your cat. Try again." }
+  validates :age, numericality: { in: (1..30), message: "Tell us your cat's real age. Be honest" }
+  validates :cuteness, presence: { message: "How cute is they?" }
+  validates :softness, presence: { message: "How plush is they?" }
 end
