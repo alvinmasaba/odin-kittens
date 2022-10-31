c2550039f95d2ccae57cc084e3f451b211186516
funkyman3121 <alvinmasaba@gmail.com>
2022-10-31 13:06:46 -0400

    Adds flash notices

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -15,8 +15,9 @@ class KittensController < ApplicationController
     @kitten = Kitten.new(kitten_params)
 
     if @kitten.save
-      redirect_to @kitten
+      redirect_to @kitten, notice: "Kitten Successfully Created!"
     else
+      flash.now[:notice] = "Couldn't Create Kitty :("
       render :new, status: :unprocessable_entity
     end
   end
@@ -29,8 +30,9 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
 
     if @kitten.update(kitten_params)
-      redirect_to @kitten
+      redirect_to @kitten, notice: "Kitten Successfully Updated!"
     else
+      flash.now[:notice] = "Oops! Couldn't Update Kitty :("
       render :edit, status: :unprocessable_entity
     end
   end
@@ -40,7 +42,7 @@ class KittensController < ApplicationController
 
     @kitten.destroy
 
-    redirect_to root_path, status: :see_other
+    redirect_to root_path, status: :see_other, notice: "Kitty gone, dead and gone..."
   end
 
   private
